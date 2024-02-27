02fec05f5b775998022f7c5e858bcad1eaf7a812
Al-Amin Jamil A. Cunanan <aacunanan1@up.edu.ph>
2024-02-27 16:24:54 +0800

    Exercise 1

---
+++ Exercise1/Exercise1_Cunanan.py
@@ -0,0 +1,5 @@
+"""
+Hi my name is AJ Cunanan
+This is GE 120
+You are watching the Disney Channel
+"""
---
+++ Lecture2_Cunanan.py
@@ -0,0 +1,33 @@
+print ("Kumusta Mundo")
+print(type("Kumusta Mundo"))
+
+print(type(str(1)))
+
+print('Wazzup' + ' GE 120')
+
+print(type("string"))
+print(type(54))
+print(type(3.1415205385))
+print(type(1+4j))
+
+print(type(float(54)))
+
+x = 2
+y = 13
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x**y)
+print(x%y)
+print(abs(y))
+print(float(y))
+print(complex(y))
+print(divmod(y,x))
+
+dog = ["golden retriever", "husky", "aspin", "poodle",]
+print(dog)
+print(dog(0))
+
