435fa8c145feff274d8ff5601b90006be2c9cb54
Al-Amin Jamil A. Cunanan <aacunanan1@up.edu.ph>
2024-03-19 11:24:19 +0800

    Submit lecture 4

---
+++ Lecture4_Cunanan.py
@@ -0,0 +1,13 @@
+def shout ():
+    print (word + "!")
+    print ("I Created this Function")
+
+word = "Hello"
+
+shout ()
+
+def shout (word1, word2):
+    print(word1+"!")
+    print(word2+"....")
+
+    shout ("wow")
